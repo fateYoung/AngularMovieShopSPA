21e619cfa0e01ad3fcca2563580f286b26880330
fateYoung <you@example.comyybh9510@gmail.com>
2019-12-09 15:28:41 -0500

    adding directories and needed components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,39 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GenresComponent } from './genres/genres.component';
+import { HomeComponent } from './home/home.component';
+import { MovieListComponent } from './shared/components/movie-list/movie-list.component';
+import { CreateUserComponent } from './account/create-user/create-user.component';
+import { MyAccountComponent } from './account/my-account/my-account.component';
+import { CreateMovieComponent } from './admin/create-movie/create-movie.component';
+import { CreateCastComponent } from './admin/create-cast/create-cast.component';
+import { CreateCrewComponent } from './admin/create-crew/create-crew.component';
+import { MyMovieComponent } from './account/my-movie/my-movie.component';
+import { MyFavoritesComponent } from './account/my-favorites/my-favorites.component';
+import { TopRatedMoviesComponent } from './movies/top-rated-movies/top-rated-movies.component';
+import { TopRevenueMoviesComponent } from './movies/top-revenue-movies/top-revenue-movies.component';
+import { HeaderComponent } from './core/layout/header/header.component';
+import { FooterComponent } from './core/layout/footer/footer.component';
+import { AboutUsComponent } from './shared/components/about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GenresComponent
+    GenresComponent,
+    HomeComponent,
+    MovieListComponent,
+    CreateUserComponent,
+    MyAccountComponent,
+    CreateMovieComponent,
+    CreateCastComponent,
+    CreateCrewComponent,
+    MyMovieComponent,
+    MyFavoritesComponent,
+    TopRatedMoviesComponent,
+    TopRevenueMoviesComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
