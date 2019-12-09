2ba014f2054ad6c706879291f66a7ebebb5d00af
fateYoung <you@example.comyybh9510@gmail.com>
2019-12-09 15:15:15 -0500

    adding bootstrap and ng bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
+// Angular Imports
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+// Package Imports
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+// Self Component Imports
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GenresComponent } from './genres/genres.component';
@@ -14,7 +19,8 @@ import { GenresComponent } from './genres/genres.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
