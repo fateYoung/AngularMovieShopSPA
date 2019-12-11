d3b8f3a2bbb520552760eac12d52b9725bebe436
fateYoung <you@example.comyybh9510@gmail.com>
2019-12-11 17:08:00 -0500

    css

--- src/app/movies/movies-by-genre/movies-by-genre.component.ts
+++ src/app/movies/movies-by-genre/movies-by-genre.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Movie } from 'src/app/shared/models/movie';
 import { MovieService } from 'src/app/core/services/movie.service';
@@ -12,23 +12,22 @@ import { Subscription } from 'rxjs';
 export class MoviesByGenreComponent implements OnInit {
 
   movies: Movie[];
-  myvariable: Subscription;
+  urlSegment: string;
   constructor(private route: ActivatedRoute, private movieService: MovieService) {
   }
 
   ngOnInit() {
-    this.myvariable = this.movieService.getMoviesByGenreId(this.route.snapshot.params.id)
-    .subscribe(
-      m => {
-        this.movies = m;
-        console.log(this.movies);
+    this.route.paramMap.subscribe(
+      params => {
+        this.urlSegment = this.route.snapshot.params.id;
+        this.movieService.getMoviesByGenreId(+this.urlSegment)
+        .subscribe(
+          m => {
+            this.movies = m;
+            console.log(this.movies);
+          }
+        );
       }
     );
   }
-
-  // tslint:disable-next-line: use-lifecycle-interface
-  ngOnDestroy() {
-    this.myvariable.unsubscribe();
-  }
-
 }
